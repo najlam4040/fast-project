0066461d94d713716c3006961655812f6eb582c7
najla <najilam469@gmail.com>
2022-05-24 11:51:21 +0530

    css added

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('x/', function () {
-    return view('welcome');
-});
+//Route::get('x/', function () {
+//    return view('welcome');
+//});
+
+Route::
